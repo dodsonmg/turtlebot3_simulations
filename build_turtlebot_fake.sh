43d6deb93e75f796146d448909b1cc48ce531bf4
Michael Dodson <md403@cl.cam.ac.uk>
2020-02-24 23:10:26 +0000

    generalised script for building

--- build_turtlebot_fake.sh
+++ build_turtlebot_fake.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 
-IN=/home/md403/rosserial/rosserial_turtlebot_ubuntu/turtlebot3_simulations/turtlebot3_fake/src/turtlebot3_fake_rosserial.cpp
+TARGET=$1
+
+IN=./turtlebot3_fake/src/turtlebot3_fake_rosserial.cpp
 OUT=turtlebot3_fake_rosserial
 
-g++ $IN -o $OUT \
-	-g \
-	-I/auto/homes/md403/rosserial/rosserial_turtlebot_ubuntu/rosserial_cheribsd/ros_lib \
-	-I/auto/homes/md403/rosserial/rosserial_turtlebot_ubuntu/turtlebot3_simulations/turtlebot3_fake/include \
-	-I/auto/homes/md403/rosserial/rosserial_turtlebot_ubuntu/turtlebot3_simulations/turtlebot3_fake/include/turtlebot3_fake
+if [[ "$TARGET" == "ubuntu" ]]; then
+	g++ $IN -o $OUT \
+		-g \
+		-I../rosserial_cheribsd/ros_lib \
+		-I./turtlebot3_fake/include \
+		-I./turtlebot3_fake/include/turtlebot3_fake
+elif [[ "$TARGET" == "cheri" ]]; then
+	$HOME/cheri/output/sdk/bin/cheri-unknown-freebsd-clang++ $IN -o $OUT \
+		-I../rosserial_cheribsd/ros_lib \
+		-I./turtlebot3_fake/include \
+		-I./turtlebot3_fake/include/turtlebot3_fake \
+		--sysroot=$HOME/cheri/output/sdk/sysroot128 \
+		-B$HOME/cheri/output/sdk
+fi
+
+
+
